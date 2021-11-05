4aabf56a9dee4c0e405281c025d97f3ec7e6182e
dumidu1998 <dumiduraj@gmail.com>
2021-11-05 14:25:30 +0530

    init

---
+++ Song.java
@@ -0,0 +1,68 @@
+class Song {
+
+    private String name;
+    private int time;
+    private String genre;
+
+    // with parameters constructor
+    public Song(String name, int time, String genre) {
+        this.name = name;
+        this.time = time;
+        this.genre = genre;
+    }
+
+    // copy constructor
+    public Song(Song song) {
+        this.name = song.getName();
+        this.time = song.getTime();
+        this.genre = song.getGenre();
+    }
+
+    // default constructor
+    public Song() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String toString() {
+        return "Song [name=" + genre + ", name=" + name + ", time=" + time + "]";
+    }
+
+    public boolean equals(Object inObject) {
+
+        boolean isEqual = false;
+        Song inSong = null;
+
+        if (inObject instanceof Song) {
+            inSong = (Song) inObject;
+            if (name.equals(inSong.getName()))
+                if (genre.equals(inSong.getGenre()))
+                    if (time == inSong.getTime())
+                        isEqual = true;
+        }
+        return isEqual;
+    }
+
+}
